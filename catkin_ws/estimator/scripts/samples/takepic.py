864f0aefd3bc6fd1685de65a280f591d028b0373
Tobias Bak <bak@stud.uni-heidelberg.de>
2018-12-04 13:24:16 +0100

    Update takepic.py

--- catkin_ws/estimator/scripts/samples/takepic.py
+++ catkin_ws/estimator/scripts/samples/takepic.py
@@ -1,76 +1,52 @@
-# Tobias Bak
-# here is a aplication for creating
-# positives and negatives sample
-
-print('importing cv2, os, time, threading')
-import threading
-import cv2
+#!/usr/bin/env python
+ 
+import numpy as np
+import cv2 as cv
+import glob
 import os
-import time
-print('import done')
-	
-	
-class TakePicture:
-	def __init__(self, ipath):
-		self.path = ipath
-		self.jpg = '.jpg'
-		self.cap = cv2.VideoCapture(0)
-	def checkdir(self): 
-		ndir = str(self.path) + '/'
-		print('checking dir ... %s' %ndir)
-		if os.path.exists(str(ndir)) == False:
-			os.makedirs(str(ndir))
-			print('Dir not exists, created .. %s' %ndir)
-		elif os.path.exists(str(ndir)):
-			print('Dir allready exitsts\ndone')
-		
-	def readFrame(self):
-		ok, frame = self.cap.read()
-		if ok != True:
-			print('errr.... somethng goes wrong! check your capture device!')
-			return None
-			pass
-		return frame
-		
-	def readFrameGray(self):
-		frame = self.readFrame()
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		return gray
-	
-	def takePic(self, rate=0.25, pcount=10):
-		if self.path == 'pos':
-			x,y = input('pos frame bounds in float x,y: ')
-		for i in range(pcount):
-			filename = self.path + '/' + self.path + str(i) + self.jpg
-			frame = self.readFrame()
-			if self.path == 'pos':
-				frame = self.readFrameGray()
-				posframe = frame[x:y,x:y]
-				cv2.imshow('posFrame',posframe)
-				cv2.imwrite(filename, posframe)
-			else:
-				cv2.imshow('frame',frame)
-				cv2.imwrite(filename, frame)
-			print('writing ... %s \n sleeping' % filename)		
-			time.sleep(rate)
-		return filename, pcount
-		
-if __name__ == "__main__":
-	ipath = str(input('input path; "pos" or "neg" it also set the mode: '))
-	pic = TakePicture(ipath)
-	pic.checkdir()
-	i=0
-	print('press "t" to start taking samples')
-	while(True):
-		cv2.imshow('main loop', pic.readFrameGray())
-		if cv2.waitKey(1) & 0xFF == ord('t'):
-			cv2.imshow('main loop', pic.readFrameGray())
-			filename, pcount = pic.takePic()
-			print('%d pictures stored' % pcount)
-			break		
-	# When everything done, release the capture
-	print('we are done relaseing capture device')
-	pic.cap.release()
-	cv2.destroyAllWindows()
-	print('done :)')
-	
+
+# take image for calibration chessboard
+cap = cv.VideoCapture(0)
+
+cap.set(cv.CAP_PROP_FRAME_WIDTH,320)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT,240)
+
+for i in range(50):
+	while True:
+		ok,img = cap.read()
+		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+		ret, showcorners = cv.findChessboardCorners(gray, (7,6), None)
+		cv.drawChessboardCorners(gray, (7,6), showcorners, ret)		
+		h, w = img.shape[:2]
+		size = h , w
+		showimg = cv.putText(img, str(size), (100,20),cv.FONT_HERSHEY_SIMPLEX,1, (50,170,50),2 )
+		cv.imshow('frame',gray)
+		if cv.waitKey(1) & 0xFF == ord('t'):
+			if not os.path.exists('./data/calpic'):
+				os.makedirs('./data/calpic')
+				print('creating dir...')
+			filename = './data/calpic/'+str(ret)+'calImg' + str(i) + '.jpg'
+			cv.imwrite(filename, img)
+			break
+cap.release()
+
+### take image for calibration aruco marker
+#cap = cv.VideoCapture(0)
+#dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_50)
+#for i in range(50):
+#	while True:
+#		ok,img = cap.read()
+#		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#		showcorners, showids, rejectedImgPoints	=	cv.aruco.detectMarkers(	gray, dictionary)
+#		cv.aruco.drawDetectedMarkers(gray, showcorners)		
+#		h, w = img.shape[:2]
+#		size = h , w
+#		showimg = cv.putText(gray, str(showids), (100,20),cv.FONT_HERSHEY_SIMPLEX,0.5, (50,170,50),2 )
+#		cv.imshow('frame',gray)
+#		if cv.waitKey(1) & 0xFF == ord('t'):
+#			filename = 'calImgAruco' + str(i) + '.jpg'
+#			cv.imwrite(filename, img)
+#			break
+#cap.release()
+#cv.destroyAllWindows()
+#
